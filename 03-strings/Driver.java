6cb1ae9cdc2dfd09180897cb32b06fc72b97d33d
pranabislam <islampranab@gmail.com>
2014-09-21 17:10:04 -0400

    updated with the general solution

--- 03-strings/Driver.java
+++ 03-strings/Driver.java
@@ -2,8 +2,8 @@ public class Driver {
     public static void main(String[] args){
 	String s = "pranab islam";
 	int locationOfSpace = s.indexOf(" ");
-	String firstName = s.substring(0,6);
-	String lastName = s.substring(7);
+	String firstName = s.substring(0,locationOfSpace);
+	String lastName = s.substring(locationOfSpace+1);
 	System.out.println(locationOfSpace);
         System.out.println(firstName);
 	System.out.println(lastName);
