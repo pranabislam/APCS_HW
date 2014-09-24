f7d62a13365d056d4a604fd6d31005107695b837
pranabislam <islampranab@gmail.com>
2014-09-23 21:54:02 -0400

    More StringStuff homework

---
+++ 05-stringstuff/Driver.java
@@ -0,0 +1,14 @@
+public class Driver{
+    public static void main(String[] args){
+	StringStuff test1 = new StringStuff();
+        StringStuff test2 = new StringStuff();
+        StringStuff test3 = new StringStuff();
+
+	System.out.println(test1.nonStart("boolo", "lalala"));
+        System.out.println(test2.makeAbba("boolo", "lalala"));
+	System.out.println(test3.diff21(233));
+    }
+        
+		      
+}
+			   
---
+++ 05-stringstuff/StringStuff.java
@@ -0,0 +1,18 @@
+public class StringStuff {
+    public static String nonStart(String a, String b) {
+	return a.substring(1) + b.substring(1);
+    }
+
+    public static String makeAbba(String a, String b) {
+	return a + b + b + a;
+    }
+
+    public static int diff21(int n){
+	if (n <= 21){
+	    return 21-n;
+	}
+	else {
+	    return (n - 21)*2;
+	}
+    }
+}
