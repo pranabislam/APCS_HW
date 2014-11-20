e32dd2f3f32339eadf3e49836bdadee2e48b80c2
Pranab <Islam>
2014-11-19 22:11:06 -0500

    still getting some errors but made progress

---
+++ 18-WordSearch/WordSearch.java
@@ -0,0 +1,55 @@
+public class WordSearch {
+    private char[][] board;
+
+    public WordSearch(int r, int c){
+	board = new char[r][c];
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		board[i][j]='.';
+	    }
+	}
+    }
+    public WordSearch() {
+	this(20,40);
+    }
+ 
+    public String toString(){
+	String s = "";
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		s = s + board[i][j];
+	    }
+	    s = s + "\n";
+	}
+	return s;
+    }
+
+    public void addWordH(String w, int row, int col){
+	int c = col;
+	if (c + w.length()  > board.length){
+	    throw new IndexOutOfBoundsException();
+	}
+	for (int z = c; z < z + c ; z++){
+	    if (board[row][z] == '.'){
+		for (int i=0; i < w.length();i++){
+		    board[row][c] = w.charAt(i);
+		    c++;
+		}
+	    }
+	    if (board[row][z] != '.'){
+		throw new IllegalThreadStateException();
+	    }
+	}
+    }	
+    public static void main(String[] args) {
+	WordSearch w = new WordSearch();
+	System.out.println(w);
+	w.addWordH("hello",3,15); // should work
+	//	w.addWordH("look",3,14); // test illegal overlap
+	//w.addWordH("look",3,18); // test legal overlap
+	//w.addWordH("look",-3,20); // test illegal row
+	//w.addWordH("look",3,55); // test illegal col
+	// etc
+	System.out.println(w);
+    }
+}
