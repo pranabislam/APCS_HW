6b179b26a8ad848f9f6288d41b5a1f557ba85aec
Pranab <Islam>
2014-11-20 22:01:21 -0500

    It is working and the tests pass!

--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -24,32 +24,81 @@ public class WordSearch {
 	return s;
     }
 
+    public boolean overlapHorizontal(String w, int row, int col){
+	int i = 0;
+	int c = col;
+	while (i < w.length()){
+	    if ((w.charAt(i) == board[row][c])||
+		(board[row][c] == '.')){
+		i++;
+		c++;
+	    }
+	    else {
+		return true;
+	    }
+	}
+	return false;
+    }
+     
+    public boolean overlapVertical(String w, int row, int col){
+	int i = 0;
+	int r = row;
+	while (i <w.length()){
+	    if ((w.charAt(i) == board[r][col])|| (board[r][col] == '.')){
+		i++;
+		r++;
+	    }
+	    else {
+		return true; } }
+	return false;
+    }
+	    
+	
+
+    
     public void addWordH(String w, int row, int col){
 	int c = col;
-	if (c + w.length()  > board.length){
-	    throw new IndexOutOfBoundsException();
-	}
-	for (int z = c; z < z + c ; z++){
-	    if (board[row][z] == '.'){
+        if ((w.length() + c <= board[0].length)   //word must fit in the board
+	    && (w.length() + col >= 0)&&
+	    (row >= 0 && row < board.length)){   //illegal column and row stop
+	    if (!(overlapHorizontal(w, row, col))){   //overlap false means good to go
 		for (int i=0; i < w.length();i++){
 		    board[row][c] = w.charAt(i);
 		    c++;
 		}
 	    }
-	    if (board[row][z] != '.'){
-		throw new IllegalThreadStateException();
-	    }
 	}
-    }	
+    }
+   
+  
+    
+    public void addWordV(String w, int row, int col){
+	int r = row;
+	if ((w.length() + r <=board.length) &&  //cant go beyond the rows
+	    (w.length() + row >= 0)&&   
+	    (col >= 0 && col < board.length)){  // illegal column and row stopper
+	    if (!(overlapVertical(w,row,col))){
+		for (int k = 0; k < w.length(); k++){
+		    board[r][col] = w.charAt(k);
+		    r++;
+		}}}}
+		
+	    
+    
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
-	System.out.println(w);
-	w.addWordH("hello",3,15); // should work
-	//	w.addWordH("look",3,14); // test illegal overlap
+     	//System.out.println(w);
+	w.addWordH("hello",0,15); // should work
+	// w.addWordH("zzzzzzz",3,14); // test illegal overlap
 	//w.addWordH("look",3,18); // test legal overlap
 	//w.addWordH("look",-3,20); // test illegal row
 	//w.addWordH("look",3,55); // test illegal col
-	// etc
+	
+	//	System.out.println(w.board[0]);
+	//	System.out.println(w.board.length);
+
+	w.addWordV("hueheu", 0, 15);
+	//w.addWordV("dASDASDD" ,0 , 4234324);      illegal column test. 
 	System.out.println(w);
     }
 }
