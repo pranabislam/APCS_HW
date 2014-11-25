c855a2122920c53d06b749f7527541d84ef1a563
pranabislam <islampranab@gmail.com>
2014-11-25 09:26:33 -0500

    adasd

--- 18-WordSearch/WordSearch.java
+++ 18-WordSearch/WordSearch.java
@@ -2,8 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class WordSearch {
-    private char[][] board;
+    private char[][] board, key;
     Random r = new Random();
+    private ArrayList<String> wordList = new ArrayList<String>();
+    private ArrayList<String> words = new ArrayList<String>();
 
     public WordSearch(int r, int c){
 	board = new char[r][c];
@@ -49,125 +51,6 @@ public class WordSearch {
 	return false;
     }
 
-
-    public boolean overlapHorizontal(String w, int row, int col){
-	int i = 0;
-	int c = col;
-	while (i < w.length()){
-	    if ((w.charAt(i) == board[row][c])||
-		(board[row][c] == '.')){
-		i++;
-		c++;
-	    }
-	    else {
-		return true;
-	    }
-	}
-	return false;
-    }
-
-        public boolean overlapHorizontalR(String w, int row, int col){
-	int i = 0;
-	int c = col;
-	while (i < w.length()){
-	    if ((w.charAt(i) == board[row][c])||
-		(board[row][c] == '.')){
-		i++;
-		c--;
-	    }
-	    else {
-		return true;
-	    }
-	}
-	return false;
-    }
-     
-     
-    public boolean overlapVertical(String w, int row, int col){
-	int i = 0;
-	int r = row;
-	while (i <w.length()){
-	    if ((w.charAt(i) == board[r][col])|| (board[r][col] == '.')){
-		i++;
-		r++;
-	    }
-	    else {
-		return true; } }
-	return false;
-    }
-    
-        public boolean overlapVerticalR(String w, int row, int col){
-	int i = 0;
-	int r = row;
-	while (i <w.length()){
-	    if ((w.charAt(i) == board[r][col])|| (board[r][col] == '.')){
-		i++;
-		r--;
-	    }
-	    else {
-		return true; } }
-	return false;
-    }
-    
-    public boolean overlapDiagonalSE(String w, int row, int col){
-	int i = 0;
-	int r = row;
-	int c = col;
-	while (i <w.length()){
-	    if ((w.charAt(i) == board[r][c])|| (board[r][c] == '.')){
-		i++;
-		r++;
-		c++;
-	    }
-	    else {
-		return true; } }
-	return false;
-    }
-	    
-    public boolean overlapDiagonalNE(String w, int row, int col){
-	int i = 0;
-	int r = row;
-	int c = col;
-	while (i <w.length()){
-	    if ((w.charAt(i) == board[r][c])|| (board[r][c] == '.')){
-		i++;
-		r--;
-		c++;
-	    }
-	    else {
-		return true; } }
-	return false;
-    }	
-
-        public boolean overlapDiagonalNW(String w, int row, int col){
-	int i = 0;
-	int r = row;
-	int c = col;
-	while (i <w.length()){
-	    if ((w.charAt(i) == board[r][c])|| (board[r][c] == '.')){
-		i++;
-		r--;
-		c--;
-	    }
-	    else {
-		return true; } }
-	return false;
-    }
-
-        public boolean overlapDiagonalSW(String w, int row, int col){
-	int i = 0;
-	int r = row;
-	int c = col;
-	while (i <w.length()){
-	    if ((w.charAt(i) == board[r][c])|| (board[r][c] == '.')){
-		i++;
-		r++;
-		c--;
-	    }
-	    else {
-		return true; } }
-	return false;
-    }
       
     public boolean generalErrorCheck(String w, int row, int col, int dy, int dx){
 	if (dx > 0){
@@ -198,7 +81,7 @@ public class WordSearch {
 	return true;
     }
 	
-    public void addWordGeneral(String w, int row, int col, int dy, int dx){
+    public boolean addWordGeneral(String w, int row, int col, int dy, int dx){
 	int c = col;
 	int r = row;
         if (generalErrorCheck(w, row, col, dy, dx)){
@@ -210,189 +93,6 @@ public class WordSearch {
 		    r = r + dy;
 		    
 		}
-	    }
-	}
-    }
-
-    public void addWordH(String w, int row, int col){
-	int c = col;
-        if ((w.length() + c <= board[0].length)   //word must fit in the board
-	    && (w.length() + col >= 0)&&
-	    (row >= 0 && row < board.length)){   //illegal column and row stop
-	    if (!(overlapHorizontal(w, row, col))){   //overlap false means good to go
-		for (int i=0; i < w.length();i++){
-		    board[row][c] = w.charAt(i);
-		    c++;
-		}
-	    }
-	}
-    }
-
-    public void addWordHR(String w, int row, int col){
-	int c = col;
-        if ((c - w.length() > 0)   //word must fit in the board
-	    && (w.length() + col >= 0)&&
-	    (row >= 0 && row < board.length)){   //illegal column and row stop
-	    if (!(overlapHorizontalR(w, row, col))){   //overlap false means good to go
-		for (int i=0; i < w.length();i++){
-		    board[row][c] = w.charAt(i);
-		    c--;
-		}
-	    }
-	}
-    }
-  
-    
-    public void addWordV(String w, int row, int col){
-	int r = row;
-	if ((w.length() + r <=board.length) &&  //cant go beyond the rows
-	    (w.length() + row >= 0)&&   
-	    (col >= 0 && col < board.length)){  // illegal column and row stopper
-	    if (!(overlapVertical(w,row,col))){
-		for (int k = 0; k < w.length(); k++){
-		    board[r][col] = w.charAt(k);
-		    r++;
-		}}}}
-
-   public void addWordVR(String w, int row, int col){
-	int r = row;
-	if (r - w.length() > 0) {    
-	    /* &&  //cant go beyond the rows
-	    (w.length() + row >= 0)&&   
-	    (col >= 0 && col < board.length)){  // illegal column and row stopper */
-	    if (!(overlapVerticalR(w,row,col))){
-		for (int k = 0; k < w.length(); k++){
-		    board[r][col] = w.charAt(k);
-		    r--;
-		}}}}
-		
-	    
-    public void addWordDSE(String w, int row, int col){
-	int r = row;
-	int c = col;
-       	if (w.length() + r <=board.length || w.length() + c <= board[0].length){
-	    if (!(overlapDiagonalSE(w,row,col))){
-		    for (int ff = 0; ff <w.length(); ff++){
-			board[r][c] = w.charAt(ff);
-			r++;
-			c++;
-		    }
-		}
-	}
-    }
-	
-
-    public void addWordDNE(String w, int row, int col){
-	int r = row;
-	int c = col;
-       	if (r - w.length() > 0 || w.length() + c <= board[0].length){
-	    if (!(overlapDiagonalNE(w,row,col))){
-		    for (int ff = 0; ff <w.length(); ff++){
-			board[r][c] = w.charAt(ff);
-			r--;
-			c++;
-		    }
-		}
-	}
-    }
-
-    public void addWordDNW(String w, int row, int col){
-	int r = row;
-	int c = col;
-       	if (r - w.length() > 0 || c - w.length() > 0){
-	    if (!(overlapDiagonalNW(w,row,col))){
-		    for (int ff = 0; ff <w.length(); ff++){
-			board[r][c] = w.charAt(ff);
-			r--;
-			c--;
-		    }
-		}
-	}
-    }
-	
-
-
-    public void addWordDSW(String w, int row, int col){
-	int r = row;
-	int c = col;
-       	if (w.length() + r <=board.length || c - w.length() > 0){
-	    if (!(overlapDiagonalNW(w,row,col))){
-		    for (int ff = 0; ff <w.length(); ff++){
-			board[r][c] = w.charAt(ff);
-			r++;
-			c--;
-		    }
-		}
-	}
-    }		
-	    
-	    /*	if ((w.length() + r <=board.length) &&  //cant go beyond the rows
-	    (w.length() + row >= 0)&&   
-	    (col >= 0 && col < board.length)){  // illegal column and row stopper
-	    if (!(overlapVertical(w,row,col))){
-		for (int k = 0; k < w.length(); k++){
-		    board[r][col] = w.charAt(k);
-		    r++;
-		    }}}}*/
-
-
-    public boolean addWord(String w){
-        int dy = -1 + r.nextInt(3);
-	int dx = -1 + r.nextInt(3);
-	if (dx == 0 && dy == 0){
-	    return false;
-	}
-	return true;
-    }
-    public boolean addWord1(String w){
-	int value  = r.nextInt(8);
-	int randrow = r.nextInt(20);
-	int randcol = r.nextInt(40);
-	if (value == 0){
-	    addWordH(w,randrow,randcol);
-	    if (!overlapHorizontal(w,randrow, randcol)){
-		return true;
-	    }
-	}
-	if (value == 1){
-	    addWordHR(w,randrow,randcol);
-	    if (!overlapHorizontalR(w,randrow, randcol)){
-		return true;
-	    }
-	}
-	if (value == 2){
-	    addWordV(w,randrow,randcol);
-	    if (!overlapVertical(w,randrow, randcol)){
-		return true;
-	    }
-	}
-	if (value == 3){
-	    addWordVR(w,randrow,randcol);
-	    if (!overlapVerticalR(w,randrow, randcol)){
-		return true;
-	    }
-	}
-	if (value == 4){
-	    addWordDNE(w,randrow,randcol);
-	    if (!overlapDiagonalNE(w,randrow, randcol)){
-		return true;
-	    }
-	}
-	if (value == 5){
-	    addWordDNW(w,randrow,randcol);
-	    if (!overlapDiagonalNW(w,randrow, randcol)){
-		return true;
-	    }
-	}
-	if (value == 6){
-	    addWordDSE(w,randrow,randcol);
-	    if (!overlapDiagonalSE(w,randrow, randcol)){
-		return true;
-	    }
-	}
-	if (value == 7){
-	    addWordDSW(w,randrow,randcol);
-	    if (!overlapDiagonalSW(w,randrow, randcol)){
 		return true;
 	    }
 	}
@@ -400,9 +100,24 @@ public class WordSearch {
     }
 
 
+    public void buildPuzzle(int numwords){
+	for (int i=0;i<numwords;i++){
+	    int wordIndex = r.nextInt(wordList.size());
+	    String w = wordList.get(wordIndex);
+	    if (addWordGeneral(w)){
+		words.add(w);
+		wordList.remove(wordIndex);
 
-
- 
+    public boolean addWord(String w){
+        int dy = -1 + r.nextInt(3);
+	int dx = -1 + r.nextInt(3);
+	if (dx == 0 && dy == 0){
+	    return false;
+	}
+	
+	return true;
+    }
+    
     public static void main(String[] args) {
 	WordSearch w = new WordSearch();
      	//System.out.println(w);
@@ -412,22 +127,7 @@ public class WordSearch {
 	w.addWordGeneral("llllllllllllllll",18,25,0,1); // should work overlap check works
 	//	w.addWordGeneral("hello",6,6,1,1); // should work
 	//	w.addWordHR("hello", 0,15);
-	// w.addWordH("zzzzzzz",3,14); // test illegal overlap
-	//w.addWordH("look",3,18); // test legal overlap
-	//w.addWordH("look",-3,20); // test illegal row
-	//w.addWordH("look",3,55); // test illegal col
-	
-	//	System.out.println(w.board[0]);
-	//	System.out.println(w.board.length);
-
-	//	w.addWordVR("hueheu", 7, 3);
-	/*	w.addWordDSE("halal", 0, 15);
-	w.addWordDSE("uuuh", 14, 10); //tests illegal overlap
-	w.addWordDSW("djjh", 10, 10); //tests illegal overlap
-	w.addWordDNW("djjjh", 10, 10); //tests illegal overlap
-	w.addWordDNE("FHH", 10, 10); //tests illegal overlap
-	w.addWordDNE("dfd", 10, 10); //tests illegal overlap
-	*/
+        
 	//w.addWordV("dASDASDD" ,0 , 4234324);      illegal column test. 
 	//	w.addWord("lo000");
 	//	System.out.println(w.generalErrorCheck("llafa", 4, 23, -1, -1));
